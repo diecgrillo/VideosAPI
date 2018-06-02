558b46b640bbbe7e0328ff7245cca8aecf64566e
Diego Grillo <diecgrillo@gmail.com>
2018-06-02 19:30:59 +1200

    connection with atlas server

--- app.js
+++ app.js
@@ -9,29 +9,9 @@ var mongoose = require('mongoose');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
-var mongoURLLabel = "";
-var dbDetails = new Object();
+var mongoURL = "mongodb+srv://video_api:video_api@cluster0-phwcy.mongodb.net/video-api-db";
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
+console.log(mongoURL);
 
 if (mongoURL == null){
 	mongoURL = 'mongodb://127.0.0.1:27017/video-api-db';
@@ -49,13 +29,6 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     // we're connected!
     console.log("Connected correctly to server");
-
-		dbDetails.databaseName = db.databaseName;
-		dbDetails.url = mongoURLLabel;
-		dbDetails.type = 'MongoDB';
-
-		console.log('Connected to MongoDB at: %s', mongoURL);
-
 });
 
 var app = express();
